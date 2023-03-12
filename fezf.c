a286c1f34ae7f303e41f8da226133a8f98468af5
NathanOD <nathan.odic@mines-ales.org>
2023-03-12 03:49:44 +0100

    finished part 1

--- fezf.c
+++ fezf.c
@@ -1,65 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_CHOICES 5
-#define MAX_CAPACITY 250
-
-// Structure de données pour représenter une famille
-struct Family {
-    int num_people;
-    int choices[MAX_CHOICES];
-    int assigned_choice; // choix affecté (0 si aucun choix n'a été affecté)
-};
-
-// Fonction qui affecte une famille à un choix donné si la capacité maximale n'est pas atteinte
-// Retourne 1 si l'affectation a réussi, 0 sinon
-int assign_family(struct Family* family, int choice, int capacity[]) {
-    if (capacity[choice] + family->num_people <= MAX_CAPACITY) {
-        family->assigned_choice = choice;
-        capacity[choice] += family->num_people;
-        return 1;
-    }
-    return 0;
-}
-
-// Fonction principale
-int main() {
-    // Exemple de tableau de familles
-    struct Family families[] = {
-        {5, {3, 6, 1, 4, 0}, 0},
-        {4, {1, 4, 2, 0, 6}, 0},
-        {2, {2, 5, 1, 0, 4}, 0},
-        {3, {0, 6, 5, 1, 4}, 0},
-        {7, {6, 5, 3, 0, 2}, 0}
-    };
-    int num_families = sizeof(families) / sizeof(struct Family);
-
-    // Capacité maximale pour chaque choix
-    int capacity[MAX_CHOICES] = {0};
-
-    // Affecter chaque famille à son premier choix, s'il est possible
-    for (int i = 0; i < num_families; i++) {
-        int assigned = 0;
-        for (int j = 0; j < MAX_CHOICES; j++) {
-            if (assign_family(&families[i], families[i].choices[j], capacity)) {
-                assigned = 1;
-                break;
-            }
+double penalite = 0.0;
+    float list_pen[7];
+    int ind_max = 0;
+    for (int j = 0; j < 7; j++) {
+        double pen = 0.0;
+        if (j < 6)
+        {
+            pen = ((choix[pos][j]-125)/400.0)*pow(choix[pos][j], abs(choix[pos][j]-choix[pos][j+1])/50.0);
         }
-        if (!assigned) {
-            printf("Impossible d'affecter la famille %d à un choix\n", i+1);
+        else
+        {
+            pen = ((choix[pos][j]-125)/400.0)*pow(choix[pos][j], abs(choix[pos][j]-choix[pos][j])/50.0);
         }
-    }
-
-    // Afficher les affectations de chaque famille
-    for (int i = 0; i < num_families; i++) {
-        printf("Famille %d : ", i+1);
-        if (families[i].assigned_choice == 0) {
-            printf("non affectée\n");
-        } else {
-            printf("affectée au choix %d\n", families[i].assigned_choice);
+        list_pen[j] = pen;
+        penalite += pen;
+        
+        if (pen>list_pen[ind_max])
+        {
+            ind_max = j;
         }
+        printf("%f ", list_pen[j]);
     }
-
-    return 0;
-}
+    printf("\n");
+    printf("%f", penalite);
+    printf("\n");
+    printf("%d", ind_max);
--- main3.c
+++ main3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define MAX_FAMILIES 500 // Maximum number of families
 #define MAX_CAPACITY 250
@@ -13,15 +14,61 @@ struct Family {
     int assigned_choice;
 };
 
-// Fonction qui affecte une famille à un choix donné si la capacité maximale n'est pas atteinte
-// Retourne 1 si l'affectation a réussi, 0 sinon
-int assign_family(struct Family* family, int choice, int capacity[]) {
-    if (capacity[choice] + family->num_people <= MAX_CAPACITY) {
-        family->assigned_choice = choice;
-        capacity[choice] += family->num_people;
-        return 1;
+int calculerCout(int choix, int nmb) {
+    int cout = 0;
+    switch (choix) {
+        case 1:
+            cout = 0;
+            break;
+        case 2:
+            cout = 50;
+            break;
+        case 3:
+            cout = 50 + 9 * nmb;
+            break;
+        case 4:
+            cout = 100 + 9 * nmb;
+            break;
+        case 5:
+            cout = 200 + 9 * nmb;
+            break;
     }
-    return 0;
+    return cout;
+}
+
+double calculerPenalite(int *pop) {
+    double penalite = 0.0;
+    for (int i = 0; i < 7; i++) {
+        double pen = 0.0;
+        if (i < 6)
+        {
+            pen = ((pop[i]-125)/400.0)*pow(pop[i], abs(pop[i]-pop[i+1])/50.0);
+        }
+        else
+        {
+            pen = ((pop[i]-125)/400.0)*pow(pop[i], abs(pop[i]-pop[i])/50.0);
+        }
+        penalite += pen;
+    }
+    return penalite;
+}
+
+void assign_families(struct Family families[], int num_families, int capacity[]) {
+    // Affecter les familles à leur choix préféré (jusqu'à ce que la capacité soit atteinte)
+    for (int i = 0; i < num_families; i++) {
+        int j;
+        for (j = 0; j < MAX_CHOICES; j++) {
+            int choice = families[i].choices[j];
+            if (capacity[choice] >= families[i].num_people) {
+                families[i].assigned_choice = choice;  // mettre à jour le choix de la famille
+                capacity[choice] -= families[i].num_people;  // mettre à jour la capacité
+                break;
+            }
+        }
+        if (j == MAX_CHOICES) {
+            families[i].assigned_choice = -1;  // marquer la famille comme "sacrifiée"
+        }
+    }
 }
 
 // Fonction qui échange deux éléments dans un tableau de familles
@@ -73,7 +120,7 @@ int compute_cost(struct Family* families, int num_families) {
 }
 
 int main() {
-    char filename[] = "pb40.csv";
+    char filename[] = "pb50.csv";
     FILE *fp;
     char line[100];
     char *token;
@@ -111,53 +158,80 @@ int main() {
     
     // Trier le tableau de familles en utilisant le tri rapide
     quicksort(families, 0, num_families-1);
-
-    /*
-    // Afficher le tableau de familles trié
-    for (int i = 0; i < num_families; i++) {
-        printf("%d : %d %d %d %d %d\n", families[i].id, families[i].choices[0], families[i].choices[1], families[i].choices[2], families[i].choices[3], families[i].choices[4]);
-    }
-    */
-
     
-    // Capacité maximale pour chaque choix
-    int capacity[MAX_CHOICES] = {0};
+    int capacities[7] = {250, 250, 250, 250, 250, 250, 250};
+    assign_families(families, num_families, capacities);
 
-    // Affecter chaque famille à son premier choix, s'il est possible
     for (int i = 0; i < num_families; i++) {
-        int assigned = 0;
-        for (int j = 0; j < MAX_CHOICES; j++) {
-            if (assign_family(&families[i], families[i].choices[j], capacity)) {
-                assigned = 1;
-                break;
-            }
-        }
-        if (!assigned) {
-            printf("Impossible d'affecter la famille %d à un choix\n", i+1);
+        if (families[i].assigned_choice == -1) {
+            // Les familles sacrifiées sont ajoutées à leur second choix
+            families[i].assigned_choice = families[i].choices[1];
         }
     }
-    /*
-    // Afficher les affectations de chaque famille
-    for (int i = 0; i < num_families; i++) {
-        printf("Famille %d : ", families[i].id);
-        printf("affectee au choix %d\n", families[i].assigned_choice);
-    }
 
-    int cost = compute_cost(families, num_families);
-    printf("%d ", cost);
-    */
-
-    // Print out the families
-    
+    // Print
     for (i = 0; i < num_families; i++) {
-        //printf("Family %d: %d people, choices: ", i+1, families[i].num_people);
         printf("Family %d: %d people, assigned at %d, choices: ", families[i].id, families[i].num_people, families[i].assigned_choice);
         for (j = 0; j < MAX_CHOICES; j++) {
             printf("%d ", families[i].choices[j]);
         }
         printf("\n");
     }
-    
+
+    int pers_jour[7];
+    for (i = 0; i < num_families; i++) {
+        switch (families[i].assigned_choice) {
+        case 0:
+            pers_jour[0] += families[i].num_people;
+            break;
+        case 1:
+            pers_jour[1] += families[i].num_people;
+            break;
+        case 2:
+            pers_jour[2] += families[i].num_people;
+            break;
+        case 3:
+            pers_jour[3] += families[i].num_people;
+            break;
+        case 4:
+            pers_jour[4] += families[i].num_people;
+            break;
+        case 5:
+            pers_jour[5] += families[i].num_people;
+            break;
+        case 6:
+            pers_jour[6] += families[i].num_people;
+            break;
+        }
+    }
+    printf("\n");
+    printf("Nombre de personnes affectees le lundi : %d\n", pers_jour[0]);
+    printf("Nombre de personnes affectees le mardi : %d\n", pers_jour[1]);
+    printf("Nombre de personnes affectees le mercredi : %d\n", pers_jour[2]);
+    printf("Nombre de personnes affectees le jeudi : %d\n", pers_jour[3]);
+    printf("Nombre de personnes affectees le vendredi : %d\n", pers_jour[4]);
+    printf("Nombre de personnes affectees le samedi : %d\n", pers_jour[5]);
+    printf("Nombre de personnes affectees le dimanche : %d\n", pers_jour[6]);
+    printf("\n");
+
+    int cout_tot = 0;
+    for (i = 0; i < num_families; i++) {
+        int choix = 0;
+        for (j = 0; j < MAX_CHOICES; j++) {
+            if (families[i].choices[j] == families[i].assigned_choice)
+            {
+                choix = j;
+            }
+        }
+        int cout = calculerCout(choix, families[i].num_people);
+        cout_tot += cout;
+    }
+    printf("Cout : %d\n", cout_tot);
+
+    double penalite = calculerPenalite(pers_jour);
+    printf("Penalite : %f\n", penalite);
+
+    printf("Fonction objectif : %f\n", cout_tot + penalite);
 
     return 0;
 }
